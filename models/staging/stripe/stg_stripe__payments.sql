908470cfd750d66bed0024df55c3a0ba7885160c
Zaahir Dawood <zaahirdawood@gmail.com>
2024-10-16 07:38:58 +0000

    update stg_stripe__payments to pull _batched_at field then write a custom data test that has successful passed the assertions

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -6,6 +6,7 @@ select
 
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
-    created as created_at
+    created as created_at,
+    _batched_at
 
 from {{ source('stripe', 'payment') }}
---
+++ tests/assert_stg_payments_amount_is_poitive.sql
@@ -0,0 +1,12 @@
+
+with payment as (
+
+    select * from {{ ref('stg_stripe__payments') }}
+)
+
+
+select order_id,
+        sum(amount) as total_amount
+from payment
+group by order_id
+having total_amount < 0
