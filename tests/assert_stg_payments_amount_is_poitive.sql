022c0af56ecd0b5156b377ae725f19da764458d2
Zaahir Dawood <zaahirdawood@gmail.com>
2024-10-16 08:26:11 +0000

    create custom assertion tests

--- tests/assert_stg_payments_amount_is_poitive.sql
+++ tests/assert_stg_payments_amount_is_poitive.sql
@@ -1,12 +1,6 @@
+with payment as (select * from {{ ref("stg_stripe__payments") }})
 
-with payment as (
-
-    select * from {{ ref('stg_stripe__payments') }}
-)
-
-
-select order_id,
-        sum(amount) as total_amount
+select order_id, sum(amount) as total_amount
 from payment
 group by order_id
 having total_amount < 0
